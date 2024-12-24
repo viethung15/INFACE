f7a1ab91fe3cc7fe5a9941c9e49c5edbdf80e58f
viethung15 <hoaabban@gmail.com>
2024-12-25 00:24:04 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+cd
+cd
+apt install python
+apt-get install python
+apt install ruby
+apt-get install ruby
+apt install pip
+apt-get install pip
+pip install requests
+pip2 install requests
+gem install lolcat
+apt install figlet
+apt-get install figlet
+cd
+cd 
+cd Infect
+cd src
+chmod +x Infect
+cp Infect /data/data/com.termux/files/usr/bin
+cd
+cd
+figlet 'Infect installed' | lolcat -p 1.0
+echo 'Run command to start: Infect'
